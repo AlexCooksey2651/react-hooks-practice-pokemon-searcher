4620cf22a0d68e522eb914721e1bd8a89c75b0b3
Alex Cooksey <aecooksey2651@gmail.com>
2022-01-14 08:30:21 -0500

    commit tree 56783e9b9e671d86530ca43ed43877d794504cbf

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,11 +2,19 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemons }) {
+  const pokemonCards = pokemons.map(pokemon => {
+    return(
+      <PokemonCard key={pokemon.id} pokemonData={pokemon}/>
+    )
+  })
   return (
-    <Card.Group itemsPerRow={6}>
+    <div>
       <h1>Hello From Pokemon Collection</h1>
-    </Card.Group>
+      <Card.Group itemsPerRow={6} >
+        {pokemonCards}
+      </Card.Group>
+    </div>
   );
 }
 
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,29 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "semantic-ui-react";
 
-function PokemonForm() {
+function PokemonForm({ addNewPokemon }) {
+  const [formData, setFormData] = useState(
+    {
+      "id": "",
+      "name": "",
+      "hp": 0,
+      "sprites": {
+        "front": "",
+        "back": "",
+      },
+    }
+  )
+
+  function handleChange(event) {
+    const name = event.target.name
+    const value = event.target.value
+    
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  function handleFrontImage(event) {
+    const spritesInfo = {...formData.sprites}
+    spritesInfo.front = event.target.value
+
+    setFormData({
+      ...formData,
+      sprites: spritesInfo
+    })
+  }
+
+  function handleBackImage(event) {
+    const spritesInfo = {...formData.sprites}
+    spritesInfo.back = event.target.value
+
+    setFormData({
+      ...formData,
+      sprites: spritesInfo
+    })
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(formData)
+
+    fetch("http://localhost:3001/pokemon", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(response => response.json())
+      .then(newPokemon => addNewPokemon(newPokemon))
+  }
+
+
+
   return (
     <div>
       <h3>Add a Pokemon!</h3>
       <Form
-        onSubmit={() => {
-          console.log("submitting form...");
-        }}
+        onSubmit={handleSubmit}
       >
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
+          <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={handleChange}/>
+          <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={handleChange}/>
           <Form.Input
             fluid
             label="Front Image URL"
             placeholder="url"
             name="frontUrl"
+            onChange={handleFrontImage}
           />
           <Form.Input
             fluid
             label="Back Image URL"
             placeholder="url"
             name="backUrl"
+            onChange={handleBackImage}
           />
         </Form.Group>
         <Form.Button>Submit</Form.Button>
